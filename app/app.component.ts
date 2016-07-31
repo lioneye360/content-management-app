9a37589f087ea38f98b8d7ab160216cb09fa0bd4
lioneye360 <lioneye360@gmail.com>
2016-07-31 14:16:14 +0300

    Stage 1 complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'content-management-app',
+    template: '<h1>Content Management App</h1>'
 })
 export class AppComponent { }
